8ab88cf6bc912ccde0faf45afb0c5b2ad7fdfb35
gurwns <gurwnsdl1@gomcorp.com>
2020-12-23 11:17:05 +0900

    update

--- Example/Pods/Target Support Files/VRiX_iOS/VRiX_iOS-umbrella.h
+++ Example/Pods/Target Support Files/VRiX_iOS/VRiX_iOS-umbrella.h
@@ -10,6 +10,22 @@
 #endif
 #endif
 
+#import "GXAdManager.h"
+#import "GXADPlayer.h"
+#import "GXPlayerView.h"
+#import "Statistics.h"
+#import "GoxUtils.h"
+#import "GTGoxConstants.h"
+#import "GTGoxImporterUtil.h"
+#import "NSString+GNSURLEncodedString.h"
+#import "NSString+InvalidChar.h"
+#import "NSURL+URLParameters.h"
+#import "XMLDictionary.h"
+#import "UIColor+HexColors.h"
+#import "GTVAST.h"
+#import "GTVMAP.h"
+#import "VRiX.h"
+#import "VRiXManager.h"
 
 FOUNDATION_EXPORT double VRiX_iOSVersionNumber;
 FOUNDATION_EXPORT const unsigned char VRiX_iOSVersionString[];
